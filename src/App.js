ce1ce87dfb7c7143491bf35d173e303cad420c9c
SonaliN84 <sonali.nilekar@gmail.com>
2023-03-15 10:13:01 +0530

    Header Created

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./Component/Layout/Header";
+import { Fragment } from "react";
+function App() {
+  return (
+    <Fragment>
+       <Header/>
+    </Fragment>
+     
+  
+  );
+}
+
+export default App;
